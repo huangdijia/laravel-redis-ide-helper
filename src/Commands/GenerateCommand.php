c9c7c681547763742611e8e021a12e539c7d1398
D.J.Hwang <huangdijia@gmail.com>
2022-03-17 14:22:35 +0800

    Support laravel 9

--- src/Commands/GenerateCommand.php
+++ src/Commands/GenerateCommand.php
@@ -1,24 +1,33 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of http-client.
+ *
+ * @link     https://github.com/huangdijia/laravel-redis-ide-helper
+ * @document https://github.com/huangdijia/laravel-redis-ide-helper/blob/2.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
 namespace Huangdijia\RedisIdeHelper\Commands;
 
 use Illuminate\Console\GeneratorCommand;
-use Illuminate\Support\Facades\Redis;
 
 class GenerateCommand extends GeneratorCommand
 {
-    protected $signature   = 'ide-helper:redis';
-    protected $description = 'Generate autocompletion for redis';
+    /**
+     * @var string
+     */
+    protected $signature = 'ide-helper:redis';
 
-    protected function getStub()
-    {
-        return __DIR__ . '/stubs/_ide_helper_redis.stub';
-    }
+    /**
+     * @var string
+     */
+    protected $description = 'Generate autocompletion for redis';
 
     public function handle()
     {
         try {
-            $path    = app()->basePath() . '/_ide_helper_redis.php';
+            $path = app()->basePath() . '/_ide_helper_redis.php';
             $content = file_get_contents($this->getStub());
 
             $this->files->put($path, $content);
@@ -27,5 +36,10 @@ class GenerateCommand extends GeneratorCommand
         } catch (\Exception $e) {
             $this->warn($e->getMessage());
         }
+    }
+
+    protected function getStub(): string
+    {
+        return __DIR__ . '/stubs/_ide_helper_redis.stub';
     }
 }
--- src/RedisIdeHelperServiceProvider.php
+++ src/RedisIdeHelperServiceProvider.php
@@ -1,5 +1,13 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of http-client.
+ *
+ * @link     https://github.com/huangdijia/laravel-redis-ide-helper
+ * @document https://github.com/huangdijia/laravel-redis-ide-helper/blob/2.x/README.md
+ * @contact  huangdijia@gmail.com
+ */
 namespace Huangdijia\RedisIdeHelper;
 
 use Illuminate\Support\ServiceProvider;
@@ -8,7 +16,6 @@ class RedisIdeHelperServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
     }
 
     public function register()
@@ -16,7 +23,7 @@ class RedisIdeHelperServiceProvider extends ServiceProvider
         $this->registerCommands();
     }
 
-    protected function registerCommands()
+    protected function registerCommands(): void
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
